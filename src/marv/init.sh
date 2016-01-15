a8581d319855c9c16177d9bdf5ddc982d837c460
Florian Friesdorf <florian@ternaris.com>
2016-01-15 22:25:21 +0100

    fixed flask-restless available via https://ternaris.com/pypi

--- src/marv/init.sh
+++ src/marv/init.sh
@@ -2,8 +2,4 @@
 set -e
 test -n "$DEBUG_START_SH" && set -x
 
-# An alternative would be -e and a repo url in requirements.txt
-# This here is more efficient and can be cached
-pip install --upgrade https://github.com/ternaris/flask-restless/archive/0.17.0-45-fix.tar.gz
-
 exit 0
